a3a3734156ba78c3f30c6f354c0ac2b1b042ba98
David Dohan <git@ddohan.com>
2018-02-19 13:37:08 -0800

    Use DagreJS for graph layout

--- src/cljs/ebbinghaus/graph.cljs
+++ src/cljs/ebbinghaus/graph.cljs
@@ -26,8 +26,8 @@
   (let [color (r/atom "red")]
     (fn []
       [:circle {:r 10
-                :cx (get node "x")
-                :cy (get node "y")
+                :cx node.x
+                :cy node.y
                 :fill @color
                 :on-click (fn [] (swap! color rand_color))
                 :on-mouse-over (fn [] (println "on"))
@@ -39,24 +39,28 @@
 (defn node_text [node]
   [:text
    {
-    :x (+ 10 (get node "x"))
-    :y (+ 10 (get node "y"))
+    :x (+ 10 node.x)
+    :y (+ 10 node.y)
     :style {
             :text-anchor "left"
             }
     }
-   (get node "name")
+   node.label
    ])
 
-(defn edge_line [edge]
-  [:line {
-          :x1 (get-in edge ["source" "x"])
-          :y1 (get-in edge ["source" "y"])
-          :x2 (get-in edge ["target" "x"])
-          :y2 (get-in edge ["target" "y"])
-          :stroke-width "2px"
-          :stroke "#000"
-          }])
+(defn edge_line [g edge]
+  (let [
+        source (.node g edge.v)
+        target (.node g edge.w)
+        ]
+    [:line {
+            :x1 source.x
+            :y1 source.y
+            :x2 target.x
+            :y2 target.y
+            :stroke-width "2px"
+            :stroke "#000"
+            }]))
 
 (defn edge_text [edge]
   [:text
@@ -71,6 +75,18 @@
    ]
   )
 
+(defn add_nodes [g nodes]
+  (doseq [node nodes]
+    (.setNode g (:db/id node) (clj->js { :label (:name node) :width 10 :height 10 }))
+    )
+  g)
+
+(defn add_edges [g edges]
+  (doseq [edge edges]
+    (.setEdge g (:db/id (:source edge)) (:db/id (:target edge))))
+  g
+  )
+
 (defn render-graph [ratom]
   (let [state (r/atom {
                        :edges []
@@ -80,49 +96,31 @@
         node_ids @(p/q '[:find ?e :where [?e :name]] conn)
         nodes (for [id (map first node_ids)]
                 @(p/pull conn '[:name] id))
-        relation_ids @(p/q '[:find ?e :where [?e :relation]] conn)
-        relations (for [id (map first relation_ids)]
-                    @(p/pull conn '[:relation :source :target] id))
+        edge_ids @(p/q '[:find ?e :where [?e :relation]] conn)
+        edges (for [id (map first edge_ids)]
+                @(p/pull conn '[:relation :source :target] id))
 
-                                        ; Get node id -> idx in the node array
-                                        ; and change `links` to be relative to it.
-        id_map (into {}
-                     (for [idx (range (count nodes))]
-                       [(:db/id (nth nodes idx)) idx]))
-        relations (for [relation relations]
-                    {
-                     :relation (get relation :relation)
-                     :source (get id_map (get-in relation [:source :db/id]))
-                     :target (get id_map (get-in relation [:target :db/id]))
-                     }
-                    )
-
-        ; TODO(ddohan): Figure out a way to avoid the switching between mutable and immutable
-        nodes_js (clj->js nodes)
-        edges_js (clj->js relations)
-        force (.. js/cola
-                  (d3adaptor js/d3)
-                  (linkDistance 120)
-                  (nodes nodes_js)
-                  (links edges_js)
-                  (start 100)
+        g (-> (.. (js/dagre.graphlib.Graph.)
+                  (setGraph {})
+                  (setDefaultEdgeLabel (fn [] {}))
                   )
-        nodes_clj (js->clj nodes_js)
-        edges_clj (js->clj edges_js)
-        prepare-graph (fn [ratom] (concat (js->clj edges_js) (js->clj nodes_js)))
+              (add_nodes nodes)
+              (add_edges edges)
+              )
         ]
+    (js/dagre.layout g)
     [:svg {:width WIDTH :height HEIGHT}
      [:g {:transform "translate(250,250)"}
-      (for [node nodes_clj]
-        [:g {:key (get node "index")}
-         [node_svg node]
-         [node_text node]
-         ])
-      (for [idx (range (count edges_clj))]
-        (let [edge (get edges_clj idx)]
-          [:g {:key idx}
-           [edge_line edge]
-           [edge_text edge]
+      (for [node (.nodes g)]
+        (let [info (.node g node)]
+          [:g {:key info.label}
+           [node_svg info]
+           [node_text info]
            ]))
+      (for [edge (.edges g)]
+        [:g {:key (str edge.v edge.w)}
+         [edge_line g edge]
+         [edge_text g edge]
+         ])
       ]
      ]))
