15f8fa7539d634abed5f9afa76dc9172a4cbc201
David Dohan <git@ddohan.com>
2018-02-20 22:42:01 -0800

    Switch to cytoscape to fix the graph layout

--- src/cljs/ebbinghaus/graph.cljs
+++ src/cljs/ebbinghaus/graph.cljs
@@ -22,12 +22,12 @@
       (.attr "height" HEIGHT)
       (.style "background-color" "grey")))
 
-(defn node_svg [node]
+(defn node_svg [pos]
   (let [color (r/atom "red")]
     (fn []
       [:circle {:r 10
-                :cx node.x
-                :cy node.y
+                :cx (:x pos)
+                :cy (:y pos)
                 :fill @color
                 :on-click (fn [] (swap! color rand_color))
                 :on-mouse-over (fn [] (println "on"))
@@ -36,63 +36,81 @@
        ])
     ))
 
-(defn node_text [node]
+(defn node_to_position [node]
+  (let [pos (.-position (.-_private node))]
+    {
+     :x (* 100 pos.x)
+     :y (* 100 pos.y)
+     }
+    ))
+
+(defn edge_to_source [edge]
+  (.-source (.-_private edge))
+  )
+
+(defn edge_to_target [edge]
+  (.-target (.-_private edge))
+  )
+
+(defn edge_to_relation [edge]
+  (.-relation (.-_private edge))
+  )
+
+(defn node_text [node pos]
   [:text
    {
-    :x (+ 10 node.x)
-    :y (+ 10 node.y)
+    :x (+ 10 (:x pos))
+    :y (+ 10 (:y pos))
     :style {
             :text-anchor "left"
             }
     }
-   node.label
+   (aget node "_private" "data" "label")
    ])
 
-(defn edge_line [g edge]
-  (let [
-        source (.node g edge.v)
-        target (.node g edge.w)
-        ]
-    [:line {
-            :x1 source.x
-            :y1 source.y
-            :x2 target.x
-            :y2 target.y
-            :stroke-width "2px"
-            :stroke "#000"
-            }]))
+(defn edge_line [source target]
+  [:line {
+          :x1 (:x source)
+          :y1 (:y source)
+          :x2 (:x target)
+          :y2 (:y target)
+          :stroke-width "2px"
+          :stroke "#000"
+          }])
 
-(defn edge_text [edge]
+(defn edge_text [edge source target]
   [:text
    {
-    :x (avg edge "x")
-    :y (avg edge "y")
+    :x (/ (+ (:x source) (:x target)) 2)
+    :y (/ (+ (:y source) (:y target)) 2)
     :style {
             :text-anchor "left"
             }
     }
-   (get edge "relation")
+   edge._private.data.relation
    ]
   )
 
 (defn add_nodes [g nodes]
   (doseq [node nodes]
-    (.setNode g (:db/id node) (clj->js { :label (:name node) :width 10 :height 10 }))
+    (.add g (clj->js {:group "nodes"
+                      :data
+                      { :id (:db/id node) :label (:name node) :width 10 :height 10 }}))
     )
   g)
 
 (defn add_edges [g edges]
   (doseq [edge edges]
-    (.setEdge g (:db/id (:source edge)) (:db/id (:target edge))))
-  g
-  )
+    (.add g (clj->js {:group "edges"
+                      :data
+                      { :id (:db/id edge)
+                       :source (:db/id (:source edge))
+                       :target (:db/id (:target edge))
+                       :width 10 :height 10 }})))
+  g)
 
-(defn render-graph [ratom]
-  (let [state (r/atom {
-                       :edges []
-                       :nodes []
-                       })
-        conn (:conn @ratom)
+(defn build_graph [ratom]
+  (let [conn (:conn @ratom)
         node_ids @(p/q '[:find ?e :where [?e :name]] conn)
         nodes (for [id (map first node_ids)]
                 @(p/pull conn '[:name] id))
@@ -100,27 +118,59 @@
         edges (for [id (map first edge_ids)]
                 @(p/pull conn '[:relation :source :target] id))
 
-        g (-> (.. (js/dagre.graphlib.Graph.)
-                  (setGraph {})
-                  (setDefaultEdgeLabel (fn [] {}))
-                  )
-              (add_nodes nodes)
-              (add_edges edges)
-              )
+        g (js/cytoscape.
+           (clj->js {
+                     :layout {
+                              :name "circle"
+                              }
+                     :elements {
+                                :nodes (clj->js (for [node nodes]
+                                                  (clj->js {:group "nodes"
+                                                            :data
+                                                            { :id (:db/id node) :label (:name node) :width 10 :height 10 }})))
+                                :edges (clj->js (for [edge edges]
+                                                  {:group "edges"
+                                                   :data
+                                                   {
+                                                    :id (:db/id edge)
+                                                    :relation (:relation edge)
+                                                    :source (:db/id (:source edge))
+                                                    :target (:db/id (:target edge))
+                                                    }}))
+                                }
+                     })
+           )
         ]
-    (js/dagre.layout g)
+    g))
+
+(defn render-graph [ratom]
+  (let [g (build_graph ratom)
+        nodes_js (.nodes g "")
+        edges_js (.edges g "")
+        ]
     [:svg {:width WIDTH :height HEIGHT}
      [:g {:transform "translate(250,250)"}
-      (for [node (.nodes g)]
-        (let [info (.node g node)]
-          [:g {:key info.label}
-           [node_svg info]
-           [node_text info]
-           ]))
-      (for [edge (.edges g)]
-        [:g {:key (str edge.v edge.w)}
-         [edge_line g edge]
-         [edge_text g edge]
-         ])
+      (for [node_id (range nodes_js.length)]
+        (let [node (aget nodes_js node_id)
+              pos (node_to_position node)
+              ]
+          [:g {:key node_id}
+           [node_svg pos]
+           [node_text node pos]
+           ]
+          ))
+      (for [edge_id (range edges_js.length)]
+        (let [edge (aget edges_js edge_id)
+              source (edge_to_source edge)
+              source_pos (node_to_position source)
+
+              target (edge_to_target edge)
+              target_pos (node_to_position target)
+              ]
+          [:g {:key edge_id}
+           [edge_line source_pos target_pos]
+           [edge_text edge source_pos target_pos]
+           ]
+          ))
       ]
      ]))
